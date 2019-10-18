325dabb2c70e737be266175c2e5c72a2579435ab
JefferyRoeder <jefferyroeder@gmail.com>
2019-10-18 15:38:08 -0500

    MVP baseline complete. Not organized

--- wrangle.py
+++ wrangle.py
@@ -48,3 +48,29 @@ def train_test(data_frame):
     # X_test = test.drop(columns='total_charges')
     # y_test = test[['total_charges']]
     # return X_train,y_train,X_test,y_test
+
+def zillow_tax():
+    df = pd.read_sql("""
+    SELECT p.taxvaluedollarcnt as tax_value, p.fips, p.taxamount, round(p.taxamount/p.taxvaluedollarcnt,4) as tax_rate 
+
+FROM propertylandusetype pl
+JOIN
+properties_2017 p ON p.propertylandusetypeid = pl.propertylandusetypeid
+JOIN
+predictions_2017 p17 ON p17.id = p.id
+WHERE 
+p.propertylandusetypeid in (279,261) 
+AND 
+(p17.transactiondate LIKE '%%2017-05%%' or p17.transactiondate LIKE '%%2017-06%%')
+AND
+p.calculatedfinishedsquarefeet IS NOT NULL
+and
+p.bedroomcnt > 0
+and 
+p.bathroomcnt > 0
+and
+p.taxvaluedollarcnt > 0
+and
+p.taxamount > 0
+""",url)
+    return df
